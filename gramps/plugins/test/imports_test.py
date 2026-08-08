ef95cdf726ac3332105fa6ab8f5952799ba83d30
Kevin White <gocaveman@gmail.com>
2026-08-08 19:01:50 -0400

    Remove blank line

--- gramps/plugins/test/imports_test.py
+++ gramps/plugins/test/imports_test.py
@@ -377,7 +377,7 @@ def make_tst_function(tstfile, file_name):
         "skipping TestImports test case %s. en_US.utf-8 locale is required."
         % (tstfile),
     )
-    
+
     def tst(self, mockptime, mocktime, mockltime, mockdtime):
         """This compares the import file with the expected result '.gramps'
         file.
